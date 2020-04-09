6e5bafa0214f59f47ea8c35df8962229c9fd843a
Ayomide Bamidele <ayoeze@hotmail.com>
2020-04-09 16:34:35 +0100

    Changed project structure

---
+++ out/shared/module.lua
@@ -0,0 +1,9 @@
+-- Compiled with https://roblox-ts.github.io v0.3.1
+-- April 9, 2020, 4:29 PM British Summer Time
+
+local exports = {};
+local function makeHello(name)
+	return "Hello from " .. name .. "!";
+end;
+exports.makeHello = makeHello;
+return exports;
